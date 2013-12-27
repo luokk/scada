0cb315d237a5e48c5f7345ec336ac27dfc8c414f
healer <healer.kx.yu@gmail.com>
2013-12-27 14:17:31 +0800

    Logger Server

--- DAQ/Scada.Logger.Server/LoggerForm.Designer.cs
+++ DAQ/Scada.Logger.Server/LoggerForm.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPageMain = new System.Windows.Forms.TabPage();
+            this.tabPageMainVision = new System.Windows.Forms.TabPage();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
@@ -36,11 +38,28 @@
             this.tabPage5 = new System.Windows.Forms.TabPage();
             this.tabPage6 = new System.Windows.Forms.TabPage();
             this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainVisionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.HpicToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.naIDeviceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.weatherToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.HvToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.IspToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.envToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dwdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ClearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tabControl.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl
             // 
+            this.tabControl.Controls.Add(this.tabPageMain);
+            this.tabControl.Controls.Add(this.tabPageMainVision);
             this.tabControl.Controls.Add(this.tabPage1);
             this.tabControl.Controls.Add(this.tabPage2);
             this.tabControl.Controls.Add(this.tabPage3);
@@ -49,93 +68,248 @@
             this.tabControl.Controls.Add(this.tabPage6);
             this.tabControl.Controls.Add(this.tabPage7);
             this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Location = new System.Drawing.Point(0, 24);
+            this.tabControl.Margin = new System.Windows.Forms.Padding(3, 3, 3, 30);
+            this.tabControl.Multiline = true;
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(634, 261);
+            this.tabControl.Size = new System.Drawing.Size(476, 524);
             this.tabControl.TabIndex = 1;
+            // 
+            // tabPageMain
+            // 
+            this.tabPageMain.Location = new System.Drawing.Point(4, 40);
+            this.tabPageMain.Name = "tabPageMain";
+            this.tabPageMain.Size = new System.Drawing.Size(468, 480);
+            this.tabPageMain.TabIndex = 7;
+            this.tabPageMain.Text = "主程序";
+            this.tabPageMain.UseVisualStyleBackColor = true;
+            // 
+            // tabPageMainVision
+            // 
+            this.tabPageMainVision.Location = new System.Drawing.Point(4, 40);
+            this.tabPageMainVision.Name = "tabPageMainVision";
+            this.tabPageMainVision.Size = new System.Drawing.Size(468, 480);
+            this.tabPageMainVision.TabIndex = 8;
+            this.tabPageMainVision.Text = "MainVision";
+            this.tabPageMainVision.UseVisualStyleBackColor = true;
             // 
             // tabPage1
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(4, 40);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(626, 235);
+            this.tabPage1.Size = new System.Drawing.Size(468, 480);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "高压电离室";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Location = new System.Drawing.Point(4, 40);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(626, 235);
+            this.tabPage2.Size = new System.Drawing.Size(468, 480);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "碘化钠谱仪";
+            this.tabPage2.Text = "NaI谱仪";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
             // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Location = new System.Drawing.Point(4, 40);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(626, 235);
+            this.tabPage3.Size = new System.Drawing.Size(468, 480);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "气象站";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // tabPage4
             // 
-            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Location = new System.Drawing.Point(4, 40);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(642, 216);
+            this.tabPage4.Size = new System.Drawing.Size(468, 480);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "超大流量气溶胶采样器";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
             // tabPage5
             // 
-            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Location = new System.Drawing.Point(4, 40);
             this.tabPage5.Name = "tabPage5";
             this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage5.Size = new System.Drawing.Size(642, 216);
+            this.tabPage5.Size = new System.Drawing.Size(468, 480);
             this.tabPage5.TabIndex = 4;
             this.tabPage5.Text = "碘采样器";
             this.tabPage5.UseVisualStyleBackColor = true;
             // 
             // tabPage6
             // 
-            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Location = new System.Drawing.Point(4, 40);
             this.tabPage6.Name = "tabPage6";
             this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage6.Size = new System.Drawing.Size(642, 216);
+            this.tabPage6.Size = new System.Drawing.Size(468, 480);
             this.tabPage6.TabIndex = 5;
             this.tabPage6.Text = "环境与安防监控";
             this.tabPage6.UseVisualStyleBackColor = true;
             // 
             // tabPage7
             // 
-            this.tabPage7.Location = new System.Drawing.Point(4, 22);
+            this.tabPage7.Location = new System.Drawing.Point(4, 40);
             this.tabPage7.Name = "tabPage7";
             this.tabPage7.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage7.Size = new System.Drawing.Size(626, 235);
+            this.tabPage7.Size = new System.Drawing.Size(468, 480);
             this.tabPage7.TabIndex = 6;
             this.tabPage7.Text = "干湿沉降采集器";
             this.tabPage7.UseVisualStyleBackColor = true;
             // 
-            // RealTimeForm
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.设置ToolStripMenuItem,
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(476, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ClearToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 设置ToolStripMenuItem
+            // 
+            this.设置ToolStripMenuItem.Checked = true;
+            this.设置ToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.设置ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MainToolStripMenuItem,
+            this.mainVisionToolStripMenuItem,
+            this.HpicToolStripMenuItem,
+            this.naIDeviceToolStripMenuItem,
+            this.weatherToolStripMenuItem,
+            this.HvToolStripMenuItem,
+            this.IspToolStripMenuItem,
+            this.envToolStripMenuItem,
+            this.dwdToolStripMenuItem});
+            this.设置ToolStripMenuItem.Name = "设置ToolStripMenuItem";
+            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.设置ToolStripMenuItem.Text = "设置";
+            // 
+            // MainToolStripMenuItem
+            // 
+            this.MainToolStripMenuItem.Checked = true;
+            this.MainToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.MainToolStripMenuItem.Name = "MainToolStripMenuItem";
+            this.MainToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.MainToolStripMenuItem.Tag = "Scada.Main";
+            this.MainToolStripMenuItem.Text = "主程序";
+            this.MainToolStripMenuItem.Click += new System.EventHandler(this.OnStripMenuItemClick);
+            // 
+            // mainVisionToolStripMenuItem
+            // 
+            this.mainVisionToolStripMenuItem.Checked = true;
+            this.mainVisionToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.mainVisionToolStripMenuItem.Name = "mainVisionToolStripMenuItem";
+            this.mainVisionToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.mainVisionToolStripMenuItem.Tag = "Scada.MainVision";
+            this.mainVisionToolStripMenuItem.Text = "MainVision";
+            this.mainVisionToolStripMenuItem.Click += new System.EventHandler(this.OnStripMenuItemClick);
+            // 
+            // HpicToolStripMenuItem
+            // 
+            this.HpicToolStripMenuItem.Checked = true;
+            this.HpicToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.HpicToolStripMenuItem.Name = "HpicToolStripMenuItem";
+            this.HpicToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.HpicToolStripMenuItem.Tag = "Scada.HPIC";
+            this.HpicToolStripMenuItem.Text = "高压电离室";
+            this.HpicToolStripMenuItem.Click += new System.EventHandler(this.OnStripMenuItemClick);
+            // 
+            // naIDeviceToolStripMenuItem
+            // 
+            this.naIDeviceToolStripMenuItem.Name = "naIDeviceToolStripMenuItem";
+            this.naIDeviceToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.naIDeviceToolStripMenuItem.Tag = "Scada.NaIDevice";
+            this.naIDeviceToolStripMenuItem.Text = "NaI谱仪";
+            this.naIDeviceToolStripMenuItem.Click += new System.EventHandler(this.OnStripMenuItemClick);
+            // 
+            // weatherToolStripMenuItem
+            // 
+            this.weatherToolStripMenuItem.Name = "weatherToolStripMenuItem";
+            this.weatherToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.weatherToolStripMenuItem.Tag = "Scada.Weather";
+            this.weatherToolStripMenuItem.Text = "气象站";
+            this.weatherToolStripMenuItem.Click += new System.EventHandler(this.OnStripMenuItemClick);
+            // 
+            // HvToolStripMenuItem
+            // 
+            this.HvToolStripMenuItem.Name = "HvToolStripMenuItem";
+            this.HvToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.HvToolStripMenuItem.Tag = "Scada.HVSampler";
+            this.HvToolStripMenuItem.Text = "超大流量气溶胶采样器";
+            this.HvToolStripMenuItem.Click += new System.EventHandler(this.OnStripMenuItemClick);
+            // 
+            // IspToolStripMenuItem
+            // 
+            this.IspToolStripMenuItem.Name = "IspToolStripMenuItem";
+            this.IspToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.IspToolStripMenuItem.Tag = "Scada.ISampler";
+            this.IspToolStripMenuItem.Text = "碘采样器";
+            this.IspToolStripMenuItem.Click += new System.EventHandler(this.OnStripMenuItemClick);
+            // 
+            // envToolStripMenuItem
+            // 
+            this.envToolStripMenuItem.Checked = true;
+            this.envToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.envToolStripMenuItem.Name = "envToolStripMenuItem";
+            this.envToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.envToolStripMenuItem.Tag = "Scada.Shelter";
+            this.envToolStripMenuItem.Text = "环境与安防监控";
+            this.envToolStripMenuItem.Click += new System.EventHandler(this.OnStripMenuItemClick);
+            // 
+            // dwdToolStripMenuItem
+            // 
+            this.dwdToolStripMenuItem.Name = "dwdToolStripMenuItem";
+            this.dwdToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.dwdToolStripMenuItem.Tag = "Scada.DWD";
+            this.dwdToolStripMenuItem.Text = "干湿沉降采集器";
+            this.dwdToolStripMenuItem.Click += new System.EventHandler(this.OnStripMenuItemClick);
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.帮助ToolStripMenuItem.Text = "帮助";
+            // 
+            // ClearToolStripMenuItem
+            // 
+            this.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem";
+            this.ClearToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ClearToolStripMenuItem.Text = "清除";
+            this.ClearToolStripMenuItem.Click += new System.EventHandler(this.ClearToolStripMenuItem_Click);
+            // 
+            // LoggerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(634, 261);
+            this.ClientSize = new System.Drawing.Size(476, 548);
             this.Controls.Add(this.tabControl);
-            this.Name = "RealTimeForm";
-            this.Text = "数据上传"; 
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "LoggerForm";
+            this.Text = " 日志分析";
             this.Load += new System.EventHandler(this.FormOnLoad);
             this.tabControl.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -149,5 +323,21 @@
         private System.Windows.Forms.TabPage tabPage5;
         private System.Windows.Forms.TabPage tabPage6;
         private System.Windows.Forms.TabPage tabPage7;
+        private System.Windows.Forms.TabPage tabPageMain;
+        private System.Windows.Forms.TabPage tabPageMainVision;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MainToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mainVisionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem HpicToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem naIDeviceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem weatherToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem HvToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem IspToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem envToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dwdToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ClearToolStripMenuItem;
     }
 }
